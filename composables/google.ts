a24d2f4d595a6e7c7422dd54c7140454dabcf807
valenciaga1231 <martin.valencic1999@gmail.com>
2024-05-08 19:56:10 +0200

    mode google map functions to different files

---
+++ composables/google.ts
@@ -0,0 +1,55 @@
+/**
+ * Function will initialize Google Map with default settings
+ *
+ * @param map_html DOM HTML element in which Google Map will be initialized
+ */
+export const initializeGoogleMap = (map_html: HTMLElement): void => {
+    if (!useGoogleMapHTML().value) {
+        console.error("google_map_html is not defined");
+        return;
+    }
+
+    // Initialize Google Map
+    useGoogleMap().value = new google.maps.Map(map_html as HTMLElement, {
+        center: { lat: 60, lng: 40 }, // Default map view position
+        zoom: 4, // Default zoom
+
+        styles: [
+            {
+                featureType: "poi",
+                stylers: [{ visibility: "off" }],
+            },
+        ],
+
+        clickableIcons: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        draggableCursor: "crosshair",
+        fullscreenControl: false,
+        minZoom: 2,
+
+        restriction: {
+            latLngBounds: {
+                north: 85,
+                south: -85,
+                west: -180,
+                east: 180,
+            },
+        },
+
+        gestureHandling: "greedy", // Does not need 2 fingers to move on map when using touchscreen (Not working on Firefox Mobile. Safari, not sure.)
+        keyboardShortcuts: false,
+    });
+};
+
+// Function used to center Google Map to specific coordinates
+export const centerGoogleMap = (lat: number, lng: number): void => {
+    const googleMap = useGoogleMap().value;
+
+    if (!googleMap) {
+        console.error("google_map is not defined");
+        return;
+    }
+
+    googleMap.setCenter({ lat, lng });
+};
